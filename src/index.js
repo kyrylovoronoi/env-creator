d2e66029ef1cf0bcdb47fe51a0acb0fcdd992dd7
Kyrylo Voronoi <kyrylo.voronoi@newfold.com>
2026-03-09 13:07:20 +0200

    add "create" command

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
 // get CLI arguments
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
+    console.log("Usage: env-creator <command>");
     process.exit(0);
 }
 
 const command = args[0];
 
 switch (command) {
+    // create an empty .env
+    case 'create': {
+        const envPath = path.join(process.cwd(), '.env');
+
+        if (fs.existsSync(envPath)) {
+            console.log('.env already exists');
+        } else {
+            fs.writeFileSync(envPath, '');
+            console.log('Created empty .env');
+        }
+
+        break;
+    }
+
     default:
         console.error('Unknown command');
         process.exit(1);
